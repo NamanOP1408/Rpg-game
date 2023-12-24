d6271e65f8412560dd60d4c2a2ed1fb7f7b6d865
Naman <chauhannaman242@gmail.com>
2023-12-24 22:37:38 +0530

    javaScript started Homies!

---
+++ index.js
@@ -0,0 +1,11 @@
+const goStore = document.querySelector('#button1');
+const goCave = document.querySelector('#button2');
+const fightDragon = document.querySelector('#button3');
+const xpText = document.querySelector('#xpText');
+const healthText = document.querySelector('#healthText');
+const goldText = document.querySelector('#goldText');
+
+goStore.addEventListener('click', () => {
+    
+
+});
